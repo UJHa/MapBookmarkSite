8c5bc3fb7fa38f79092e33dbe335cd4a36d76c97
UHA <ddkk94@naver.com>
2021-04-12 13:25:55 +0900

    Marker 테이블 변경
    지번 주소에 대한 문자열의 컬럼 추가

--- mapBookmarkSite/mapbookmark/models.py
+++ mapBookmarkSite/mapbookmark/models.py
@@ -8,6 +8,7 @@ class Member(models.Model):
 
 class Marker(models.Model):
     member_id = models.ForeignKey(Member, on_delete=models.CASCADE)
+    address_name = models.CharField(max_length=200, default='')
     latitude = models.FloatField(default=0.0)
     longitude = models.FloatField(default=0.0)
     title = models.CharField(max_length=100, default='')
